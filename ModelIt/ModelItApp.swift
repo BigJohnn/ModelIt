0791799f5d86faa018467d4631e424d39c0e6d7e
BigJohhn <kjustdoitno1@gmail.com>
2023-07-13 11:58:25 +0800

    Initial Commit

---
+++ ModelIt/ModelItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelItApp.swift
+//  ModelIt
+//
+//  Created by HouPeihong on 2023/7/13.
+//
+
+import SwiftUI
+
+@main
+struct ModelItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
